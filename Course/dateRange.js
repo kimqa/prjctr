9ec0275d1ef4f4cbb4a8a6e414deeb9f53ee8d00
unknown <kimskad@gmail.com>
2025-01-26 21:51:03 +0200

    add .js switcher

--- Course/dateRange.js
+++ Course/dateRange.js
@@ -1,5 +1,8 @@
+"use strict"
+console.log("dateRange.js loaded");
+
 function dateRange(start, end, unit) {
-    const #startInTimestamp = new Date(start);
-    const #endInTimestamp = new Date(end);
-    let range = Math.abs( #endInTimestamp -["#startInTimestamp"])
+    const startInTimestamp = new Date(start);
+    const endInTimestamp = new Date(end);
+    let range = Math.abs( endInTimestamp -startInTimestamp);
 }
